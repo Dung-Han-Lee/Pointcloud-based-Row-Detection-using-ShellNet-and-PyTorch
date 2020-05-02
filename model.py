04653e599bb67ec058ecd9b7364263426e06143d
Dung-Han-Lee <dhl77771@gmail.com>
2020-05-01 21:49:54 -0400

    implemented shell convolution module

--- model.py
+++ model.py
@@ -3,14 +3,10 @@ import torch
 import torch.nn.functional as F
 import pdb
 
-# TODO 
-# experiment with small example to test 
-# batchnorm3d
-# maxpool2d
-# conv2d
-
 def knn_one_batch(points, queries, K):
     """
+    This implementation is an inefficient workaround.
+
     Args:
         points: L x N x 3 tensor
         query : L x M x 3 tensor, M < N
@@ -36,6 +32,8 @@ def knn_one_batch(points, queries, K):
 
 def knn(points, queries, K):
     """
+    This implementation is an inefficient workaround.
+
     Args:
         points: B x L x N x 3 tensor
         query : B x L x M x 3 tensor, M < N
@@ -58,23 +56,21 @@ def knn(points, queries, K):
 
 def gather_feature(feat, indices):
     """
-    This function uses the indices generated in knn to gather associated
-    feature in the previous layer. This implementation is an inefficient
-    workaround.
+    This implementation is an inefficient workaround.
 
     Args:
-        input  : B x L x N x K x F tensor
-        indices: B x L x N x K tensor
+        input    ( B x L x N x F tensor) -- feature from previous layer
+        indices  ( B x L x M x K tensor) --  represents queries' k nearest neighbor
     Output:
-
+        features ( B x L x M x K x F tensor) -- features from previous layer 
+        that are associated with k nearest neighbors of queries
     """
-
     res = None
-    for B, point2d in enumerate(test_input):
+    for B, point_feature_2d in enumerate(feat):
         one_batch = None
-        for L, point1d in enumerate(point2d):
-            pt = point1d[indices[B][L]].unsqueeze(0)
-            one_batch = pt if one_batch is None else torch.cat((one_batch, pt))
+        for L, point_feature_1d in enumerate(point_feature_2d):
+            pf = point_feature_1d[indices[B][L]].unsqueeze(0)
+            one_batch = pf if one_batch is None else torch.cat((one_batch, pf))
         one_batch = one_batch.unsqueeze(0)
         res = one_batch if res is None else torch.cat((res, one_batch))
     return res
@@ -83,7 +79,13 @@ def gather_feature(feat, indices):
 class Lift(nn.Module):
     def __init__(self, in_size, out_size, is_batchnorm=True):
         super(Lift, self).__init__()    
-
+        """
+        Args:
+            input ( L x N x K x 3  tensor ) -- subtraction vectors 
+                from query to its k nearest neighbor
+        Output: 
+            local point feature ( B x L x N x K x 64 tensor ) 
+        """
         self.is_batchnorm = is_batchnorm
 
         self.bn1 = nn.BatchNorm3d(3)
@@ -108,17 +110,43 @@ class Lift(nn.Module):
 
 
 class ShellConv(nn.Module):
-    def __init__(self, num_feature, num_neighbor, num_division, is_batchnorm=True):
-        super(Lift, self).__init__() 
+    def __init__(self, out_features, prev_features, neighbor, division, 
+            is_batchnorm=True):
+        super(ShellConv, self).__init__() 
+        """
+        out_features  (int) num of output feature (dim = -1)
+        prev_features (int) num of prev feature (dim = -1)
+        neighbor      (int) num of nearest neighbor in knn
+        division      (int) num of division
+        """
 
-        s = int(num_neighbor/num_division)
-        self.K = num_neighbor
-        self.C = num_feature
-        self.lift = Lift(3, 64)
-        self.maxpool = nn.Maxpool3d((1, s, 1), stride = (1,s,1))
+        self.K = neighbor
+        self.S = int(self.K/division) # num of feaure per shell
+        self.point_feature = 64
+        self.neighbor = neighbor
+        in_channel = self.point_feature + prev_features
+        out_channel = out_features
 
-    def forward(self, points, queries, feat_prev):
+        self.lift = Lift(3, self.point_feature)
+        self.maxpool = nn.MaxPool3d((1, self.S, 1), stride = (1, self.S, 1))
+        if is_batchnorm == True:
+            self.conv = nn.Sequential(
+                nn.BatchNorm3d(in_channel),
+                nn.Conv3d(in_channel, out_channel, (1, 1, division))
+            )
+        else:
+            self.conv = nn.Conv3d(in_channel, out_channel, (1, 1, division))
         
+    def forward(self, points, queries, feat_prev):
+        """
+        Args:
+            points      (B x L x N x 3 tensor)
+            query       (B x L x M x 3 tensor) -- note that M < N
+            feat_prev   (B x L x N x F1 tensor)
+        Outputs:
+            feat        (B x L x M x F2 tensor)
+        """
+
         nn_pts, idxs = knn(points, queries, self.K)
         nn_center    = queries.unsqueeze(3)
         nn_points_local = nn_center - nn_pts
@@ -128,18 +156,21 @@ class ShellConv(nn.Module):
         if feat_prev is not None:
             feat_prev = gather_feature(feat_prev, idxs)
             feat_cat  = torch.cat((nn_feat_local, feat_prev), dim = -1)
-        else
+        else:
             feat_cat  = nn_feat_local
 
         feat_max = self.maxpool(feat_cat)
-        return feat_max
+        feat_max = feat_max.permute(0,4,1,2,3) # BHWKF -> BFHWK
+        output   = self.conv(feat_max).permute(0,2,3,4,1)
+
+        return output.squeeze(3)
 
 
-B , N, M = 1, 32, 1024
+B , N, M = 1, 16, 128
 # Create random Tensors to hold inputs and outputs
 p = torch.randn(B, N, M, 3)
 q = torch.randn(B, N, M//2, 3)
-
+f = torch.randn(B, N, M, 128)
 # Construct our model by instantiating the class defined above
 
 test_input = torch.zeros(2,3,4)
@@ -159,31 +190,15 @@ queries = queries.permute(0,2,1).unsqueeze(0)
 test_input = p
 value, indices = knn(test_input, q, 32)
 
-#print(value.shape)
-#print(indices.shape)
-#res = gather_feature(test_input, indices)
-#print(torch.all(torch.eq(value, res)))
-#print(knn(test_input, queries, 2).shape)
+model = ShellConv(out_features=256, prev_features=128, neighbor=32, division=4)
+res = model(test_input, q, f)
+y = torch.randn(B, N, M//2, 256)
 
-
-model = Lift(3, 64, True)
-#print(model(test_input, queries).shape)
-
-nn_pts, indices = knn(p, q, 32)
-nn_center = q.unsqueeze(3)
-nn_pts_local = nn_center - nn_pts
-
-print(model(nn_pts_local).shape)
-y = torch.randn(1,N, M//2, 32, 64)
-
-# Construct our loss function and an Optimizer. The call to model.parameters()
-# in the SGD constructor will contain the learnable parameters of the two
-# nn.Linear modules which are members of the model.
 criterion = torch.nn.MSELoss(reduction='sum')
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 for t in range(100):
     # Forward pass: Compute predicted y by passing x to the model
-    y_pred = model(nn_pts_local)
+    y_pred = model(test_input, q, f)
     # Compute and print loss
     loss = criterion(y_pred, y)
     #if t % 100 == 99:
