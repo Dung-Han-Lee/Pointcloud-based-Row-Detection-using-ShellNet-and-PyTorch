7855393277239e2cb34ca304ff2f1456eaf52a68
Dung-Han-Lee <dhl77771@gmail.com>
2020-05-07 00:13:37 -0400

    use IOU as metric for eval

--- evaluation/evaluate.py
+++ evaluation/evaluate.py
@@ -22,22 +22,23 @@ class Test:
         
         # Initialize model
         weight = torch.load(path_weight, map_location = self.device)
-        self.network = model.ShellNet(2, 1024)
+        self.network = model.ShellNet(2, 1024, conv_scale=2, dense_scale=2)
         self.network.load_state_dict(weight)
         self.network.eval()
         self.network.to(self.device)
 
     def evaluate(self, test_loader):
 
-        angle = np.random.uniform(0, 0)
+        angle = pi/6#np.random.uniform(0, 0)
         rot_z = np.array([  [cos(angle), -sin(angle), 0],
                             [sin(angle),  cos(angle), 0],
                             [         0,           0, 1]])
 
+        acc = []
         for num, (pointcloud, labels) in enumerate(test_loader):
             (pointcloud, labels) = map(lambda x : x.to(self.device), (pointcloud, labels))
 
-            angle = np.random.uniform(-pi/10, pi/10)
+            angle = pi/30 #np.random.uniform(-pi/10, pi/10)
             rot_z = np.array([  [cos(angle), -sin(angle), 0],
                                 [sin(angle),  cos(angle), 0],
                                 [         0,           0, 1]])
@@ -47,10 +48,14 @@ class Test:
             output_class = np.argmax(output, axis=2).flatten()
             pc = pointcloud.squeeze(0).detach().numpy()
             labels = labels.squeeze(0).detach().numpy()
-            show_semantic(output_class, pc, view='top')
-            show_semantic(labels      , pc, view='top', color='g')
-            plt.show()
+            intersect = np.sum(labels.astype(np.bool) & output_class.astype(np.bool))
+            union    = np.sum(labels.astype(np.bool) | output_class.astype(np.bool))
 
+            acc.append(intersect/union)
+            #show_semantic(output_class, pc, view='top')
+            #show_semantic(labels      , pc, view='top', color='g')
+            #plt.show()
+        print(np.mean(acc))
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Evalutate results of range image unet.')
