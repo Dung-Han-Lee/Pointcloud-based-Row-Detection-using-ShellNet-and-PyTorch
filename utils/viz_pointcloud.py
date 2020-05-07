21b801fd921ab098054d9b347e10bb60b6083dbd
Dung-Han-Lee <dhl77771@gmail.com>
2020-05-06 21:45:29 -0400

    compare with ground truth

--- utils/viz_pointcloud.py
+++ utils/viz_pointcloud.py
@@ -6,7 +6,7 @@ import sys
 import cv2
 import os
 
-def show_semantic(label, pointcloud, view='front', color='r'):
+def show_semantic(label, pointcloud, ground_truth=0, view='front', color='r'):
     """
     Args:
         label      : (N, ) numpy semantic annotation of target of two classes [0, 1]
@@ -22,6 +22,9 @@ def show_semantic(label, pointcloud, view='front', color='r'):
     # Draw class 1
     pc2 = pointcloud[label==1, :]
     ax.scatter(pc2[:, 0], pc2[:, 1], pc2[:, 2], s=3, c=color, marker='o')
+
+    pc3 = pointcloud[ground_truth==1, :]
+    ax.scatter(pc3[:, 0], pc3[:, 1], pc3[:, 2], s=3, c='g', marker='o')
 
     print("pc1 = {}, pc2 = {}, pointcloud = {}".format(pc1.shape, pc2.shape, pointcloud.shape))
 
